bc4dc0c262ded54f84f4d8e80fb61b34ab566bdc
Lucas Carvalho <lucascarvalhopc@gmail.com>
2025-08-07 15:44:53 -0300

    implemented mat-table for task listing

--- frontend/src/app/components/task-list/task-list.ts
+++ frontend/src/app/components/task-list/task-list.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { TaskItem } from '../task-item/task-item';
 import { Task } from '../../models';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-task-list',
-  imports: [TaskItem],
+  imports: [MatTableModule],
   templateUrl: './task-list.html',
   styleUrl: './task-list.css'
 })
 export class TaskList {
+  displayedColumns = ['id', 'title', 'user', 'actions'];
+
   tasks: Task[] = [
     {
       id: 1,
@@ -16,14 +19,22 @@ export class TaskList {
       description: "Task asdfas asdf",
       priority: 'HIGH',
       status: 'IN_PROGRESS',
-      deadline: new Date()
+      deadline: new Date(),
+      user: {
+        name: 'John Doe',
+        email: 'john.doe@example.com'
+      }
     },
     {
       id: 2,
       title: "Task 2",
       priority: 'LOW',
       status: 'IN_PROGRESS',
-      deadline: new Date()
+      deadline: new Date(),
+      user: {
+        name: 'Jane Doe',
+        email: 'jane.doe@example.com'
+      }
     }
   ]
 }
--- frontend/src/app/models/task.model.ts
+++ frontend/src/app/models/task.model.ts
@@ -1,9 +1,12 @@
+import { User } from "./user.model";
+
 export interface Task {
     id: number,
     title: string,
     description?: string,
     priority: 'LOW' | 'MEDIUM' | 'HIGH',
     status: 'DONE' | 'IN_PROGRESS',
-    deadline?: Date
+    deadline?: Date,
+    user: User
 }
 
